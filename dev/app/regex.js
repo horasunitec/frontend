2359b68bb8bce063b0c20610094a3a2d0af36abd
horasunitec <horasunitec@gmail.com>
2018-03-30 00:49:45 -0600

    F-2 Email registration regex update

--- dev/app/regex.js
+++ dev/app/regex.js
@@ -1,5 +1,5 @@
 module.exports = {
-	email: /^[A-Za-z\d]+(_|\.|\-)?[A-Za-z\d]+@unitec\.edu$/,
+	email: /^[A-Za-z\d]+(_|\.|\-)?[A-Za-z\d]+((@unitec\.edu)|(@unitec\.edu\.hn))$/,
 	password: /^(?=.*\d)(?=.*[A-Z]).+$/,
 	accountId: /^\d+$/,
 	name: /^[A-Z]{1}[a-z\-]+$/
