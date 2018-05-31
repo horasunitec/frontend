1db5e3537ff8f63c79cd51f554e2b237c8e7ef37
horasunitec <horasunitec@gmail.com>
2018-05-31 00:26:24 -0600

    email regex correccion

--- dev/app/regex.js
+++ dev/app/regex.js
@@ -1,5 +1,5 @@
 module.exports = {
-	email: /^[A-Za-z\d]+(_|\.|\-)?[A-Za-z\d]+((@unitec\.edu)|(@unitec\.edu\.hn))$/,
+	email: /^[a-zA-Z0-9]+([._]?[a-zA-Z0-9])+((@unitec\.edu)|(@unitec\.edu\.hn))$/,
 	password: /^(?=.*\d)(?=.*[A-Z]).+$/,
 	accountId: /^\d+$/,
 	name: /^[A-Z]{1}[a-z\-]+$/
